0a0cd68e30f35a62791d6000b87e0e2a35ef3a8f
Marece <wbmmusic@gmail.com>
2022-11-07 09:22:38 -0600

    0.2.17

--- public/utilities.js
+++ public/utilities.js
@@ -6,7 +6,6 @@ var AdmZip = require("adm-zip");
 const SerialPort = require('serialport')
 const { usb } = require('usb');
 const messenger = require('./messenger');
-const { locoObjects } = require('./main'); //
 
 // PATHS
 const pathToDataFolder = join(app.getPath('userData'), 'data')
